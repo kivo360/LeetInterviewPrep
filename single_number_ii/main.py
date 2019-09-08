2fc1e35391a95620edaf12004c7c6b31bd4a9514
Administrator <admin@example.com>
2019-09-08 19:23:29 -0400

    Started added comments to the code. Only 4 lines, so not too bad.

--- single_number_ii/main.py
+++ single_number_ii/main.py
@@ -3,8 +3,12 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # Counter is a godsend for these kinds of problems.
+        # It gets the number of occurences of each item in the list in the most efficient manner
         number_counts = Counter(nums)
+        # Now that we have the number of occurences we're iterating through them here.
         for k, v in number_counts.items():
-            if v == 1:
-                return k
+            # We return the index for the number that has only a single occurence.
+            if v == 1: 
+                return k # this is the value of the single occurence.
         return 0
