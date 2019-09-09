36eeecf8baa25d21750d3f9b6ec91df5845794a3
Administrator <admin@example.com>
2019-09-08 21:45:58 -0400

    Added many problems through copy and paste. Retyping and re-explaining tomorrow morning.

---
+++ binary_tree_order_traversal/main.py
@@ -0,0 +1,16 @@
+from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        ans, level = [], [root]
+        while root and level:
+            ans.append([node.val for node in level])
+            LRpair = [(node.left, node.right) for node in level]
+            level = [leaf for LR in LRpair for leaf in LR if leaf]
+        return ans
---
+++ unique_paths/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @return an integer
+    def uniquePaths(self, m, n):
+        aux = [[1 for x in range(n)] for x in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                aux[i][j] = aux[i][j-1]+aux[i-1][j]
+        return aux[-1][-1]
